7df5575a0cecd1c9552460df880a1869a7023ef0
wellingtonmacena <wellington.macena.23@gmail.com>
2020-12-13 17:56:30 -0300

    Refactored project structure

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module 'calculate-coordinates' {
+
+    export function fromCoordinatesReturningKM(centerCoordinates: [], extremeCoordinates: []): number
+
+    export function fromCoordinatesReturningKM(centerCoordinates: [], extremeCoordinates: []): number
+}
